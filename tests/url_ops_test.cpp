e130d79d45bdd34d7aa1cd53a0431fdf455446f5
Alborz Jafari <alborz.jf@yandex.com>
2020-10-18 16:28:18 +0330

    Improve UrlOps unit tests for last changes.

--- tests/url_ops_test.cpp
+++ tests/url_ops_test.cpp
@@ -32,6 +32,12 @@ INSTANTIATE_TEST_CASE_P(
                  80, "127.0.0.1", "/dir/subdir/", Protocol::HTTP),
       make_tuple("http://127.0.0.1:85/file.dat", "file.dat",
                  85, "127.0.0.1", "/", Protocol::HTTP),
+      make_tuple("http://www.exam-ple.com:1234/dir/subdir/file", "file",
+                 1234, "www.exam-ple.com", "/dir/subdir/", Protocol::HTTP),
+      make_tuple("http://www.example.com:1234/di-r/sub-dir/fi-le", "fi-le",
+                 1234, "www.example.com", "/di-r/sub-dir/", Protocol::HTTP),
+      make_tuple("http://www.exam-p.le.com:1234/di-r/sub-dir/fi-le", "fi-le",
+                 1234, "www.exam-p.le.com", "/di-r/sub-dir/", Protocol::HTTP),
 
       make_tuple("https://example.com/dir/subdir/file.dat", "file.dat",
                  443, "example.com", "/dir/subdir/", Protocol::HTTPS),
@@ -47,6 +53,12 @@ INSTANTIATE_TEST_CASE_P(
                  1234, "127.0.0.1", "/dir/subdir/", Protocol::HTTPS),
       make_tuple("https://127.0.0.1:85/file.dat", "file.dat",
                  85, "127.0.0.1", "/", Protocol::HTTPS),
+      make_tuple("https://www.exam-ple.com:1234/dir/subdir/file", "file",
+                 1234, "www.exam-ple.com", "/dir/subdir/", Protocol::HTTPS),
+      make_tuple("https://www.example.com:1234/di-r/sub-dir/fi-le", "fi-le",
+                 1234, "www.example.com", "/di-r/sub-dir/", Protocol::HTTPS),
+      make_tuple("https://www.exam-p.le.com:1234/di-r/sub-dir/fi-le", "fi-le",
+                 1234, "www.exam-p.le.com", "/di-r/sub-dir/", Protocol::HTTPS),
 
       make_tuple("ftp://example.com/dir/subdir/file.dat", "file.dat",
                  21, "example.com", "/dir/subdir/", Protocol::FTP),
@@ -61,7 +73,13 @@ INSTANTIATE_TEST_CASE_P(
       make_tuple("ftp://127.0.0.1:1234/dir/subdir/fi.le", "fi.le",
                  1234, "127.0.0.1", "/dir/subdir/", Protocol::FTP),
       make_tuple("ftp://127.0.0.1:85/file.dat", "file.dat",
-                 85, "127.0.0.1", "/", Protocol::FTP)
+                 85, "127.0.0.1", "/", Protocol::FTP),
+      make_tuple("ftp://www.exam-ple.com:1234/dir/subdir/file", "file",
+                 1234, "www.exam-ple.com", "/dir/subdir/", Protocol::FTP),
+      make_tuple("ftp://www.example.com:1234/di-r/sub-dir/fi-le", "fi-le",
+                 1234, "www.example.com", "/di-r/sub-dir/", Protocol::FTP),
+      make_tuple("ftp://www.exam-p.le.com:1234/di-r/sub-dir/fi-le", "fi-le",
+                 1234, "www.exam-p.le.com", "/di-r/sub-dir/", Protocol::FTP)
       ));
 
 TEST_P(UrlOpsTest, url_ops_should_return_right_values)
